7f1868b3c098701aad5cd0fe9bc6b01d1d88fd5c
angel0chek <d.danilenko768@gmail.com>
2026-05-30 21:27:50 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,9 @@
 //     error logger plugins, and sandbox detection (port/host/strictPort).
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-
 export default defineConfig({
-  tanstackStart: {
-    // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
-    // nitro/vite builds from this
-    server: { entry: "server" },
-  },
-});
+  server: {
+    host: '0.0.0.0', // слушать все сетевые интерфейсы
+    port: 5173       // здесь можно жестко зафиксировать порт
+  }
+})
